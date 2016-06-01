59d793091723ea46308729e9a4c334e34097ea63
Ignacio Jara <ijara756@gmail.com>
2016-05-31 22:13:56 -0400

    listado de mensajes

--- client/mensajes/Mensajes.js
+++ client/mensajes/Mensajes.js
@@ -1,2 +1,9 @@
 Meteor.subscribe('mensajes');
 Meteor.subscribe('tags');
+Meteor.subscribe('users');
+
+Template.ListadoMensajes.helpers({
+  mensajes: ()=> {
+       return Mensajes.find({empresa: Meteor.userId()},{sort:{createdAt:-1}});
+  }
+});
