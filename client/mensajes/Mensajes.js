6be7236c8114442c7a0389b772f4e02b53bd5586
Ignacio Jara <ijara756@gmail.com>
2016-06-03 19:29:58 -0400

    limpiando el codigo y ordenando las vistas

--- client/mensajes/Mensajes.js
+++ client/mensajes/Mensajes.js
@@ -8,11 +8,6 @@ Template.ListadoMensajes.helpers({
 Template.MCT.helpers({
   mensajes: function() {
     var tag = FlowRouter.getParam('tag');
-    var items =  Mensajes.find({tags:tag},{sort:{createdAt:-1}});
-    var fecha;
-    items.forEach(function (item) {
-      item.createdAt = moment(item.createdAt).fromNow();
-    });
-    return items;
+    return Mensajes.find({$and:[{conectado:true},{tags:tag}]},{sort:{createdAt:-1}});
   }
 });
