08cba818f5aeb27633130f2d71f0c1be527fbcdc
Ignacio Jara <ijara756@gmail.com>
2016-06-21 16:37:05 -0400

    experimentando con admin

--- client/organizaciones/Organizaciones.js
+++ client/organizaciones/Organizaciones.js
@@ -3,3 +3,9 @@ Template.Organizaciones.helpers({
        return Meteor.users.find({});
   }
 });
+Template.DetalleOrg.helpers({
+  organizacion: function() {
+    var id = FlowRouter.getParam('id');
+    return Meteor.users.find({_id:id});
+  }
+});
